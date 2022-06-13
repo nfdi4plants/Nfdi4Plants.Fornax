9132222438f0e703a34cabffefb99e6eec26cd26
Kevin F <Freymaurer@gmx.de>
2022-06-13 11:14:23 +0200

    Add build project :sparkles:

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,3 @@
+### 0.0.0 (Released 2022-6-13)
+* Additions:
+    * Initial set up for RELEASE_Notes.md
---
+++ build/ProjectInfo.fs
@@ -0,0 +1,38 @@
+﻿module ProjectInfo
+
+open Fake.Core
+open Fake.Extensions.Release
+
+Release.exists()
+
+let project = "Nfdi4Plants.Fornax"
+
+let testProjects = 
+    [
+        // add relative paths (from project root) to your testprojects here
+        @"tests\Fornax.Nfdi4Plants.Tests\Fornax.Nfdi4Plants.Tests.fsproj"
+    ]
+
+let solutionFile  = $"{project}.sln"
+
+let configuration = "Release"
+
+let gitOwner = "Freymaurer"
+
+let gitHome = $"https://github.com/{gitOwner}"
+
+let projectRepo = $"https://github.com/{gitOwner}/{project}"
+
+let pkgDir = "pkg"
+
+let release = ReleaseNotes.load "RELEASE_NOTES.md"
+
+let stableVersion = SemVer.parse release.NugetVersion
+
+let stableVersionTag = (sprintf "%i.%i.%i" stableVersion.Major stableVersion.Minor stableVersion.Patch )
+
+let mutable prereleaseSuffix = ""
+
+let mutable prereleaseTag = ""
+
+let mutable isPrerelease = false
