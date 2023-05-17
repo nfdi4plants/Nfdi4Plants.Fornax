ee95381b1dbac11b16c531b06ef1eae7f7d69b58
Kevin F <Freymaurer@gmx.de>
2023-05-17 15:44:21 +0200

    Add pagefind basepath

--- src/Nfdi4Plants.Fornax/SubComponents/PagefindSearchbar.fs
+++ src/Nfdi4Plants.Fornax/SubComponents/PagefindSearchbar.fs
@@ -5,19 +5,25 @@ open Html
 type PagefindSearchbar = class end
     with
     static member create (?productionBasePath: string) =
+        let productionBasePath = Option.bind (fun (p: string) -> "/" + p.Trim([|'/'; '\\'|]) |> Some) productionBasePath
         let appendBasePath (url: string) = 
             if productionBasePath.IsSome then
-                "/" + productionBasePath.Value.Trim([|'/'; '\\'|]) + url
+               productionBasePath.Value + url
             else
                 url
+        /// This is used to append links from searchresult
+        let pagefindBasePath = if productionBasePath.IsSome then productionBasePath.Value else ""
         div [HtmlProperties.Custom("slot","searchbar")] [
             link [Href <| appendBasePath "/pagefind/pagefind-ui.css"; Rel "stylesheet"]
             script [Src <| appendBasePath "/pagefind/pagefind-ui.js"; Type "text/javascript"] []
             div [Id "search"] []
             script [] [
-                !!"""window.addEventListener('DOMContentLoaded', (event) => {
-    new PagefindUI({ element: "#search" });
-});"""
+                !!(sprintf """window.addEventListener('DOMContentLoaded', (event) => {
+    new PagefindUI({ 
+        element: "#search",
+        baseUrl: "%s"
+    });
+});""" pagefindBasePath)
             ]
         ]
     
